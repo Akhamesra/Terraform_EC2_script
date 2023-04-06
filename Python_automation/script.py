2aa8c466ff6d39197c3ae30a8713cedf5a1f0302
Akhamesra <akhamesra31@gmail.com>
2023-04-06 15:29:04 +0530

    idk

--- Python_automation/script.py
+++ Python_automation/script.py
@@ -2,6 +2,7 @@ from flask.cli import AppGroup
 import click
 import boto3
 import config.settings as AppSetting
+import json
 
 s3_resource = boto3.resource('s3')
 
@@ -35,9 +36,9 @@ def uploadFile():
     except Exception as ce:
         print(ce)
 
-ses = AppGroup('SES')
+ses = AppGroup('ses')
 
-@ses.command('sendMail')
+@ses.command('sendLaunchMail')
 @click.option('--email',required=True,help="Enter Sender's email id")
 @click.option('--number_of_ec2')
 def sendMail(email,number_of_ec2):  
@@ -49,6 +50,56 @@ def sendMail(email,number_of_ec2):
         SUBJECT = "Instances Created"
         BODY_TEXT = "FROM TEAM DEVOPS"
         BODY_HTML = number_of_ec2+" EC2 instances created."
+        CHARSET = "UTF-8"
+        # client = self.getClient('ses', AWS_REGION)
+        client = boto3.client('ses', region_name=AWS_REGION)
+        try:
+            response = client.send_email(
+                Destination={
+                    'ToAddresses': RECIPIENT,
+                },
+                Message={
+                    'Body': {
+                        'Html': {
+                            'Charset': CHARSET,
+                            'Data': BODY_HTML,
+                        },
+                        'Text': {
+                            'Charset': CHARSET,
+                            'Data': BODY_TEXT,
+                        },
+                    },
+                    'Subject': {
+                        'Charset': CHARSET,
+                        'Data': SUBJECT,
+                    },
+                },
+                Source=SENDER,
+            )
+        except Exception as e:
+            print('email sent fail : '+str(e))
+            return False
+        else:
+            print('email sent')
+
+def instanceCount():
+    with open('terraform_modules/EC2/file1.json') as f:
+        data = json.load(f)
+        data = data['variables']['instance_count']['value']
+    return data
+
+@ses.command('sendTerminateMail')
+@click.option('-e','--email',required=True,help="Enter Sender's email id")
+def sendTerminateMail(email):
+        instances = instanceCount()  
+        RECIPIENT=[email]      
+        if(email.find('$$')!=-1):
+            RECIPIENT=email.split('$$')  
+        SENDER = AppSetting.sender['name'] + " <" + AppSetting.sender['email'] + ">"
+        AWS_REGION = "ap-south-1"
+        SUBJECT = "Instances Terminated"
+        BODY_TEXT = "FROM TEAM DEVOPS"
+        BODY_HTML = str(instances) +" EC2 instances terminated."
         CHARSET = "UTF-8"
         # client = self.getClient('ses', AWS_REGION)
         client = boto3.client('ses', region_name=AWS_REGION)
