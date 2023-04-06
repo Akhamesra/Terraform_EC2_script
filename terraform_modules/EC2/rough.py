bef20e0ea69f5bb7d41fb820d15ed0497be579ce
Akhamesra <akhamesra31@gmail.com>
2023-04-06 17:33:08 +0530

    fix

--- terraform_modules/EC2/rough.py
+++
@@ -1,9 +0,0 @@
-import json
-count =0 
-with open('file1.json') as f:
-    data = json.load(f)
-    for d in data['resource_changes']:
-        if d['type']=='aws_instance':
-            count+=1
-print(count)
-    
