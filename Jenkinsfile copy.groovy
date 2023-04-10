e9fd6650ddfc9bbce3c097d6765ee1e171424775
Akhamesra <akhamesra31@gmail.com>
2023-04-10 17:56:19 +0530

    changed subnets

---
+++ Jenkinsfile copy.groovy
@@ -0,0 +1,83 @@
+pipeline {
+    agent {node {label "${env.AGENT_LABEL}"}}
+    environment{
+        FLASK_APP='app'
+    }
+    stage('Setup Flask CLI Project') {
+            steps {
+                script{
+                    //Git Credentials
+                    // NonProdGitCredentialsId = 'gitlabapicaller'
+                    ProdGitCredentialsId = 'gitlabapicaller'
+
+                    FLASKCLI = "source venv/bin/activate && flask"
+                    ENVIRONMENT = "${ENVIRONMENT}"
+
+                    if(ENVIRONMENT == "PROD"){
+                        ENVIRONMENT = "prd"
+                        BUCKET_NAME = "bfsd-prod-envconfigs-mum"
+                        GitCredentialsId = ProdGitCredentialsId
+                        AWS_ARN = 574680025548
+                    }
+                    
+                }
+            }
+        }
+
+    stages {
+        stage('Code checkout'){
+            steps{
+                git branch: 'main', credentialsId: 'github', url: 'https://github.com/Akhamesra/Terraform_EC2_script.git'
+                echo 'Code Checkout Done'
+                sh 'pip3 install virtualenv' 
+              	sh 'python3 -m virtualenv venv'
+               	sh 'source venv/bin/activate && pip3 install -r requirements.txt'
+                // sh 'source venv/bin/activate && sudo yum install -y yum-utils'
+            }
+        }
+        stage('set variables'){
+            steps{
+                script{
+                    TERRAFORM = 'cd terraform_modules/EC2 && terraform '
+                    instancecount = "-var instance_count=${instance_count} "
+                    amiid = "-var ami=${AMI_ID} "
+                    instancetype = "-var instancetype=${Instance_type} "
+                    
+                    run = "${env.Run}"
+                    batch = "${env.Batch}"
+                    instancecountnumber = "${instance_count}"
+                    
+                }
+            }
+        }
+        
+        stage ("terraform apply") {
+            steps {
+                script{
+                    if(run=='Launch'){
+                        def flaskOutput = sh(returnStdout: true, script: 'flask ec2 choosesubnet -i '+instancecountnumber)
+                        def subnetid = flaskOutput.replaceAll("\\s+", "")
+                        sh "flask s3 downloadFile"
+                        sh 'echo "Batch - $(cat terraform_modules/EC2/number)" >> terraform_modules/EC2/instance_ips.txt'
+                        sh terraform+' init -reconfigure -backend-config=backend.hcl -backend-config="key=batch$(cat number)/terraform.tfstate"'
+                        sh terraform+" apply " + instancecount + amiid + instancetype + "-var subnetid=${subnetid} --auto-approve"
+                        sh "flask s3 uploadFile"
+                        sh "flask ses sendLaunchMail --number_of_ec2 "+instancecountnumber
+                    }
+                    else{
+
+                        sh terraform+ ' init -reconfigure -backend-config=backend.hcl -backend-config=key=batch'+batch+'/terraform.tfstate'
+                        sh terraform+ ' plan -destroy -out=file'
+                        sh terraform+ ' show -json file > file1.json'
+                        sh terraform+ ' destroy --auto-approve'
+                        if(batch=="1"){
+                            sh 'flask s3 deleteFile'
+                        }
+                        sh "flask ses sendTerminateMail"
+                    }
+                }
+           }
+        }
+    }
+    
+}
--- config/settings.py
+++ config/settings.py
@@ -1,7 +1,7 @@
 bucketname = "akshitkhamesraautomation"
 
-subnets = ['subnet-051895ae60c4f447d','subnet-0390f0184f9c6c81a','subnet-082071928fe3e5a8f']
-
+# subnets = ['subnet-051895ae60c4f447d','subnet-0390f0184f9c6c81a','subnet-082071928fe3e5a8f']
+subnets = ['subnet-0f40d98eed0d61400','subnet-069aa9597e563e52e']
 objectnumber = { 'name' : 'number',
                 'paths3' : '',
                 'pathlocal' : 'terraform_modules/EC2/'}
