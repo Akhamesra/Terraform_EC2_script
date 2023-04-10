b2c238b5b6dc9cf2e41742b0a951f33fc9f08e83
Akhamesra <akhamesra31@gmail.com>
2023-04-10 17:27:31 +0530

    clean

--- Python_automation/ec2_cli.py
+++ Python_automation/ec2_cli.py
@@ -12,17 +12,20 @@ ec2 = AppGroup('ec2')
 @ec2.command('choosesubnet')
 @click.option('-i', '--instance_count', required=True)
 def choosesubnet(instance_count):
-    filters = [{'Name':'subnet-id', 'Values': AppSetting.subnets}]
-    subnets = ec2_resource.subnets.filter(Filters=filters)
-    for subnet in list(subnets):
-        free_ips = subnet.available_ip_address_count
-        # n = int(subnet.cidr_block.split('/')[1])
-        # cidr_ips = 2**(32-n)
-        # used_ips = cidr_ips - free_ips
-        # print('{:s}: cidr={:d}, aws used=5, you used={:d}, free={:d}'.\
-        #     format(subnet.id, cidr_ips, used_ips - 5, free_ips))
-        if int(instance_count)<= free_ips:
-            print(subnet.id)
-            return subnet.id
-        else:
-            return False
+    try:
+        filters = [{'Name':'subnet-id', 'Values': AppSetting.subnets}]
+        subnets = ec2_resource.subnets.filter(Filters=filters)
+        for subnet in list(subnets):
+            free_ips = subnet.available_ip_address_count
+            # n = int(subnet.cidr_block.split('/')[1])
+            # cidr_ips = 2**(32-n)
+            # used_ips = cidr_ips - free_ips
+            # print('{:s}: cidr={:d}, aws used=5, you used={:d}, free={:d}'.\
+            #     format(subnet.id, cidr_ips, used_ips - 5, free_ips))
+            if int(instance_count)<= free_ips:
+                print(subnet.id)
+                return subnet.id
+            else:
+                return False
+    except Exception as e:
+        print(e)
