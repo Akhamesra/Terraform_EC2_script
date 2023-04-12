4b16d82a5e42c52ab47d7e23641ebb1ef55a2de8
Akhamesra <akhamesra31@gmail.com>
2023-04-12 11:09:38 +0530

    change dir

--- config/settings.py
+++ config/settings.py
@@ -4,11 +4,11 @@ subnets = ['subnet-051895ae60c4f447d','subnet-0390f0184f9c6c81a','subnet-0820719
 # subnets = ['subnet-0f40d98eed0d61400','subnet-069aa9597e563e52e']
 
 objectnumber = { 'name' : 'next_batch_number',
-                'paths3' : '', #s3/path
+                'paths3' : '/automation/analytics/airflow-automated-provision/', #s3/path
                 'pathlocal' : 'terraform_modules/EC2/'} #local/path
 
 objectip = { 'name' : 'instance_ips.txt',
-             'paths3' : '',   #s3/path
+             'paths3' : '/automation/analytics/airflow-automated-provision/',   #s3/path
              'pathlocal' : 'terraform_modules/EC2/'} #local/path
 
 sender = { 'name' : 'Akshit',
