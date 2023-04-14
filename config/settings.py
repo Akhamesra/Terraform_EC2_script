7d13c01a303b482999770397ffee2d30651f583e
akshitkhamesra <akshit.khamesra@bajajfinserv.in>
2023-04-14 20:19:18 +0530

    subnet

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,6 @@
 bucketname = "akshitkhamesraautomation"
 
-subnets = ['subnet-051895ae60c4f447d','subnet-0390f0184f9c6c81a','subnet-082071928fe3e5a8f']
+subnets = ['subnet-0cc3a375602ba94a5','subnet-03826aac73ce7e154']
 # subnets = ['subnet-0f40d98eed0d61400','subnet-069aa9597e563e52e']
 
 objectnumber = { 'name' : 'next_batch_number',
